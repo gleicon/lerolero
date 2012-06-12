47e6fb32f09c2d520486a7dbc2c991181b4f3ebd
Gleicon Moraes <gleicon@gmail.com>
2012-06-12 10:56:03 -0300

    initial commit

---
+++ web.py
@@ -0,0 +1,18 @@
+from bottle import get, run, debug, abort, request, ServerAdapter, template, static_file
+from lerolero import sayit
+
+import os, json
+STATIC_ROOT_PATH = "./static/"
+
+@get('/')                                                                     
+@get('/index.html')                                                           
+@get('/static/<filename:path>')                        
+def send_file(filename='index.html'):                                         
+    return static_file(filename, root=STATIC_ROOT_PATH)     
+
+@get('/lerolero')
+def lero():
+    i = sayit()
+    return i
+
+run(server='gevent', port=os.environ.get('PORT', 5000))
